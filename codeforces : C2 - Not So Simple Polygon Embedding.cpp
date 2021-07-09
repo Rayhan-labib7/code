061d9751736093d8d3d5b01a4703222ff6b91bc8
Rayhan-labib7 <87138791+Rayhan-labib7@users.noreply.github.com>
2021-07-09 12:34:25 +0600

    Create codeforces : C2 - Not So Simple Polygon Embedding.cpp

---
+++ codeforces : C2 - Not So Simple Polygon Embedding.cpp
@@ -0,0 +1,48 @@
+
+                                                                  /*--------------------|
+                                                                  |بِسْمِ الّٰلهِ الرَّحْمٰنِ الرَحِيْمِ |
+                                                                  |____________________*/
+ 
+                                                       /*--------------->Logic%2==0---------------->*/
+ 
+ 
+                                                                      //আগে যদি জানতাম
+                                                                      //      CSE
+                                                                     // তে কি আসতাম 😢*
+ 
+#include<bits/stdc++.h>
+#define CASE(t)                 printf("Case %d: ",t)
+#define gcd(a,b)                __gcd(a,b)
+#define lcm(a,b)                ((a)*(b))/gcd(a,b)
+#define PI                      2*acos(0.0)
+#define ll                      long long int
+#define pq_small              priority_queue < int, vector <int>, greater <int> > pq;
+#define endl                    '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+ 
+ 
+int main()
+{
+    optimize();
+    cout<<fixed<<setprecision(12);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+         ll n;
+ 
+         cin>>n;
+ 
+         double longest_diagonal=1/(2*(sin(PI/(n*4))));
+ 
+         cout<<longest_diagonal<<endl;
+ 
+ 
+    }
+    
+   //help this pic link : www.murderousmaths.co.uk/books/pix/formpix/longd3.gif
+ 
+/*---------->HaPpY CoDiNg----------->*/
+return 0;
+}
